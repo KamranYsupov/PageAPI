22ea7f73e48d6e09b1b8fbc762820b7cae0b5eff
KamranYsupov <kamrankek48@gmail.com>
2025-08-26 16:40:50 +0400

    inline поля админки

--- src/apps/pages/admin.py
+++ src/apps/pages/admin.py
@@ -3,14 +3,40 @@ from django.contrib import admin
 from .models import Page, Audio, Video
 
 
+class AbstractContentInline(admin.TabularInline):
+    """Абстрактный класс inline поля контента"""
+    extra = 1
+    exclude = ('counter',)
+
+    class Meta:
+        abstract = True
+
+class AudioInline(AbstractContentInline):
+    model = Audio
+
+class VideoInline(AbstractContentInline):
+    model = Video
+
+
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        VideoInline,
+        AudioInline,
+    ]
+
+
+class AbstractContentAdmin(admin.ModelAdmin):
+    """Абстрактный класс для админ-панели контента"""
+    exclude = ('counter',)
+
+    class Meta:
+        abstract = True
 
 @admin.register(Audio)
-class AudioAdmin(admin.ModelAdmin):
+class AudioAdmin(AbstractContentAdmin):
     pass
 
 @admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
+class VideoAdmin(AbstractContentAdmin):
     pass
