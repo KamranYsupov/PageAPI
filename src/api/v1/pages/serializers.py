189e10f6a1bd34b2c9a2cf8569fd59c0d0c15cb6
KamranYsupov <kamrankek48@gmail.com>
2025-08-27 18:31:33 +0400

    рефакторинг page сериализаторов

--- src/api/v1/pages/serializers.py
+++ src/api/v1/pages/serializers.py
@@ -4,43 +4,47 @@ from rest_framework import serializers
 from apps.pages.models import Page, Video, Audio
 
 
-class ContentSerializerMetaMixin:
-    exclude = ('page', )
+class BaseContentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        exclude = ('page', )
 
 
-class VideoSerializer(serializers.ModelSerializer):
-    class Meta(ContentSerializerMetaMixin):
+class VideoSerializer(BaseContentSerializer):
+    class Meta(BaseContentSerializer.Meta):
         model = Video
 
 
-class AudioSerializer(serializers.ModelSerializer):
-    class Meta(ContentSerializerMetaMixin):
+class AudioSerializer(BaseContentSerializer):
+    class Meta(BaseContentSerializer.Meta):
         model = Audio
 
 
-class BasePageSerializerMeta:
-    model = Page
-    fields = (
-        'id',
-        'title',
-    )
+class BasePageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Page
+        fields = (
+            'id',
+            'title',
+        )
 
 
-class PageListSerializer(serializers.ModelSerializer):
+class PageListSerializer(BasePageSerializer):
     detail_url = serializers.HyperlinkedIdentityField(
         view_name='api_v1:page-detail',
         lookup_field='pk',
     )
 
-    class Meta(BasePageSerializerMeta):
-        fields = BasePageSerializerMeta.fields + ('detail_url', )
+    class Meta(BasePageSerializer.Meta):
+        fields = BasePageSerializer.Meta.fields + ('detail_url', )
 
 
-class PageDetailSerializer(serializers.ModelSerializer):
+class PageDetailSerializer(BasePageSerializer):
     content = serializers.SerializerMethodField()
 
-    class Meta(BasePageSerializerMeta):
-        fields = BasePageSerializerMeta.fields + ('content', )
+    class Meta(BasePageSerializer.Meta):
+        fields = BasePageSerializer.Meta.fields + ('content', )
 
     def get_content(self, obj):
         videos = VideoSerializer(obj.videos.all(), many=True).data
